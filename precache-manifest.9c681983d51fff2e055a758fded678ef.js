aca015b03400e8522b148d11d3d5bb8bae2b08e1
santhoshnkl3 <santhoshnkl3@gmail.com>
2019-10-02 20:22:26 +0530

    Updates

---
+++ precache-manifest.9c681983d51fff2e055a758fded678ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f6e659a872566facc89c3ee91bedfcd",
+    "url": "/ReactUpdated/index.html"
+  },
+  {
+    "revision": "937dee2ee57ec62b6872",
+    "url": "/ReactUpdated/static/css/main.1cb8be26.chunk.css"
+  },
+  {
+    "revision": "c5ccd481d8811bc6d7bb",
+    "url": "/ReactUpdated/static/js/2.7cdcbf42.chunk.js"
+  },
+  {
+    "revision": "937dee2ee57ec62b6872",
+    "url": "/ReactUpdated/static/js/main.d8086e8c.chunk.js"
+  },
+  {
+    "revision": "889e451a8023b786f2c3",
+    "url": "/ReactUpdated/static/js/runtime-main.31e7c570.js"
+  }
+]);
